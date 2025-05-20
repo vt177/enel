8f29b054c7de4c0f9a07a6e02c2f0c5fb64f7a10
vt177 <vitasilvaa77@gmail.com>
2025-05-20 17:06:00 -0300

    Enable Home Assistant MQTT service integration

--- run.sh
+++ run.sh
@@ -1,15 +1,13 @@
 #!/usr/bin/with-contenv bashio
 set -e
 
-# if ! bashio::services.available "mqtt"; then
-#     bashio::log.error "No internal MQTT service not found"
-# else
-#     bashio::log.info "MQTT service found, fetching credentials ..."
-#     MQTT_HOST=$(bashio::services mqtt "host")
-#     MQTT_USER=$(bashio::services mqtt "username")
-#     MQTT_PASSWORD=$(bashio::services mqtt "password")
-
-   
-# fi
+if ! bashio::services.available "mqtt"; then
+    bashio::log.error "No internal MQTT service not found"
+else
+    bashio::log.info "MQTT service found, fetching credentials ..."
+    MQTT_HOST=$(bashio::services mqtt "host")
+    MQTT_USER=$(bashio::services mqtt "username")
+    MQTT_PASSWORD=$(bashio::services mqtt "password")
+fi
 
 APP_PORT=40002 APP_DEBUG=true APP_PREFIX=/enel APP_LOG_LEVEL=debug APP_JWT_SECRET=$(uuidgen) NODE_ENV=production node app/index.js
